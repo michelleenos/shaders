c423497be73289fad592b8c5b67f5be0520be1f9
michelle enos <32843306+crankysparrow@users.noreply.github.com>
2025-12-16 08:40:52 -0500

    tiles

--- src/glsl/tiles-motion.glsl
+++ src/glsl/tiles-motion.glsl
@@ -9,6 +9,11 @@ uniform vec2 u_resolution;
 uniform float u_time;
 uniform float u_duration;
 
+uniform float u_divideBase;
+uniform float u_divideMult;
+
+uniform vec2 u_2ndPartOffset;
+
 // #define PI 3.14159265358979323846
 #ifndef PI
 #define PI 3.1415926535897932384626433832795
@@ -58,54 +63,50 @@ void main() {
 	vec2 st1 = st;
 	vec2 st2 = st;
 
-	st1 *= 8.0;
+	st1 *= u_divideBase * u_divideMult;
 	st1 = fract(st1);
 	st1 -= 0.5;
 	st1 *= 2.0;
 
-	st2 *= 4.0;
-	st2 += vec2(0.5, 0.5);
+	st2 *= u_divideBase;
+	// st2 += vec2(0.5);
+	st2 += u_2ndPartOffset;
 	st2 = fract(st2);
 	st2 -= 0.5;
 	st2 *= 2.0;
-	// st *= 4.0;
-	// st = fract(st);
-
-	// st -= 0.5;
-	// st *= 2.0;
 
 	float c = 0.0;
-	// vec2 st2 = sta1;
-	// vec2 st3 = sta1;
+
+	float dur = u_duration;
 
 	float time = u_time;
-	float t = mod(time, u_duration) / u_duration;
 
-	// t = 0.05;
-	// float t1 = min(map(t, 0.0, 0.5, 0.0, 1.0), 1.0);
-	float t1 = mapTime(t, 0.0, 0.5);
+	float t = mod(time, dur) / dur;
+
+	float tr_adj = sin(st.x * 0.5 + st.y * 0.5);
+
+	float t1 = mod(time, dur) / dur;
+	t1 = mapTime(t1, 0.0, 0.7);
 	float t2 = mapTime(t, 0.5, 1.0);
+	// float t_rot = mapTime(t, 0.0, 0.75);
+	// float t_rotr_adj = sin(st.x * 0.3 + st.y * 0.5);
+	// float t_rot = mod(time + t_rotr_adj, dur) / dur;
+	// t_rot += abs(sin(st.x * 0.9));
+	float t_rot = mod(time + tr_adj, dur) / dur;
+	t_rot = mapTime(t_rot, 0.35, 0.7);
 
 	float c1 = 0.0;
 	vec2 st1a = st1;
 
-	// t1 = 0.1 / t1;
-	// t1 = pow(t1, 2.0);
-	// t1 = quinticOut(t1);
-	st1a *= rotate2d(PI * 0.25);
+	// t1 = quarticOut(t1);
+	st1a *= rotate2d(PI * 0.25 + PI * 0.5 * t_rot);
 	c1 = rect(st1a, vec2(1.0) * t1);
 
-	// vec2 st1b = st1;
-	// st1b += vec2(-0.75, 0.0);
-	// st1b *= rotate2d(PI * 0.25);
-	// c1 += rect(st1b, vec2(1.0) * t1);
-
 	float c2 = 0.0;
-	// st2 += vec2(0.0, -1.0);
-	st2 *= rotate2d(PI * 0.25);
-	c2 = rect(st2, vec2(2.0) * t2);
+	st2 *= rotate2d(PI * 0.25 - PI * 0.5 * t2);
+	c2 = rect(st2, vec2(1.0) * t2 + vec2(0.4));
 
 	c = c1 - c2;
 
-	gl_FragColor = vec4(vec3(step(1.3, t1)), 1.0);
+	gl_FragColor = vec4(vec3(c), 1.0);
 }
--- src/uniforms/tiles-motion.ts
+++ src/uniforms/tiles-motion.ts
@@ -3,10 +3,29 @@ import { type ShaderUniform, type ShaderInfo } from '../types/types'
 
 const uniforms = {
     u_duration: {
-        value: 3.0,
+        value: 2.0,
         min: 0,
         max: 10,
         step: 0.1,
+    },
+    u_divideBase: {
+        value: 4.0,
+        min: 1,
+        max: 20,
+        step: 1,
+    },
+    u_divideMult: {
+        value: 2.0,
+        min: 1.0,
+        max: 5.0,
+        step: 1,
+    },
+    u_2ndPartOffset: {
+        value: new THREE.Vector2(0.5, 0.5),
+        min: -1,
+        max: 1,
+        step: 0.1,
+        hide: true,
     },
 } satisfies { [key: string]: ShaderUniform }
 
