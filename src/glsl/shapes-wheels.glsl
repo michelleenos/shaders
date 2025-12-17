f5e02b4da99369f0a9900c03e8f451f6682fc624
michelle enos <32843306+crankysparrow@users.noreply.github.com>
2025-12-16 23:15:42 -0500

    wheels

--- src/glsl/shapes-wheels.glsl
+++ src/glsl/shapes-wheels.glsl
@@ -9,8 +9,13 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 uniform float u_outerFreq;
+uniform float u_useOuter2;
+uniform float u_outerFreq2;
+uniform float u_outerSpeed1;
+uniform float u_outerSpeed2;
 uniform float u_outerEdge1;
 uniform float u_outerEdge2;
+uniform float u_outerMult;
 uniform float u_outerSize;
 uniform float u_innerFreqBig;
 uniform float u_innerFreqSmall;
@@ -18,6 +23,9 @@ uniform float u_innerSpeedBig;
 uniform float u_innerSpeedSmall;
 uniform float u_innerOffset;
 uniform float u_innerMult;
+
+uniform float u_outer;
+uniform float u_inner;
 
 void main() {
 
@@ -28,25 +36,32 @@ void main() {
   float r = length(pos) * 2.0;
   float angle = atan(pos.y, pos.x);
 
-  float iu = 1.0;
-  iu = cos(angle * u_outerFreq + u_time);
-  // iu = 1.0 - smoothstep(0.5, 2.0, iu)
-  iu = 1.0 - smoothstep(u_outerEdge1, u_outerEdge2, iu);
-  iu *= u_outerSize;
+  float outr = 1.0;
+  outr = cos(angle * u_outerFreq + u_time * u_outerSpeed1);
 
-  float ou = 1.0;
-  //   ou = sin(angle * 10.0 - u_time * u_innerSpeedBig);
-  ou = sin(angle * u_innerFreqBig - u_time * u_innerSpeedBig);
-  //   ou *= 0.05;
-  ou *= u_innerMult; // 0.05
-  //   ou *= cos(angle * 5.0 - u_time * 7.0);
-  ou *= cos(angle * u_innerFreqSmall - u_time * u_innerSpeedSmall);
-  ou += u_innerOffset; // 0.45
+  float outer2 = sin(angle * u_outerFreq2 - u_time * u_outerSpeed2);
+  // outer2 *= u_outerMult;
+  outr *= mix(1.0, outer2, u_useOuter2);
+  outr *= u_outerMult;
+  // outr *= u_outerMult;
+  // outr *= sin(angle * u_outerFreq2 - u_time * u_outerSpeed2);
+  // outr = 1.0 - smoothstep(0.5, 2.0, outr)
+  outr = 1.0 - smoothstep(u_outerEdge1, u_outerEdge2, outr);
+  outr *= u_outerSize;
 
-  iu = smoothstep(iu, iu + 0.01, r);
-  ou = 1.0 - smoothstep(ou, ou + 0.01, r);
+  float inr = 1.0;
+    // inr = sin(angle * 10.0 - u_time * u_innerSpeedBig);
+  inr = sin(angle * u_innerFreqBig - u_time * u_innerSpeedBig);
+  //   inr *= 0.05;
+  inr *= u_innerMult;
+  //   inr *= cos(angle * 5.0 - u_time * 7.0);
+  inr *= cos(angle * u_innerFreqSmall - u_time * u_innerSpeedSmall);
+  inr += u_innerOffset; // 0.45
 
-  color = vec3(ou + iu);
+  outr = smoothstep(outr, outr + 0.01, r);
+  inr = 1.0 - smoothstep(inr, inr + 0.01, r);
+
+  color = vec3(inr * u_inner + outr * u_outer);
 
   gl_FragColor = vec4(color, 1.0);
 }
--- src/uniforms/shapes-wheels.ts
+++ src/uniforms/shapes-wheels.ts
@@ -1,12 +1,6 @@
 import { type ShaderInfo } from '../types/types'
 
 const uniforms = {
-    u_outerFreq: {
-        value: 9.0,
-        min: 0,
-        max: 20,
-        step: 1,
-    },
     u_outerEdge1: {
         value: 0.5,
         min: -5,
@@ -25,6 +19,39 @@ const uniforms = {
         max: 1,
         step: 0.01,
     },
+    u_outerFreq: {
+        value: 9.0,
+        min: 0,
+        max: 20,
+        step: 1,
+    },
+    u_outerSpeed1: {
+        value: 1.0,
+        min: -10,
+        max: 10,
+        step: 0.1,
+    },
+
+    u_useOuter2: { value: false },
+    u_outerFreq2: {
+        value: 7.0,
+        min: 0,
+        max: 20,
+        step: 1,
+    },
+    u_outerSpeed2: {
+        value: 1.0,
+        min: -10,
+        max: 10,
+        step: 0.1,
+    },
+    u_outerMult: {
+        value: 1,
+        min: -1,
+        max: 1,
+        step: 0.01,
+    },
+
     u_innerFreqBig: {
         value: 10.0,
         min: 0,
@@ -61,10 +88,33 @@ const uniforms = {
         max: 1,
         step: 0.01,
     },
+    u_outer: { value: true },
+    u_inner: { value: true },
 }
 
 const shaderInfo: ShaderInfo<keyof typeof uniforms> = {
     uniforms,
+    presets: [
+        {
+            u_outerFreq: 4,
+            u_useOuter2: true,
+            u_outerFreq2: 9,
+            u_outerSpeed1: -5.2,
+            u_outerSpeed2: -0.1,
+            u_outerMult: -0.57,
+            u_outerEdge1: 0.7,
+            u_outerEdge2: -2,
+            u_outerSize: 1,
+            u_innerFreqBig: 12,
+            u_innerFreqSmall: 5,
+            u_innerSpeedBig: 1,
+            u_innerSpeedSmall: 3.9,
+            u_innerOffset: 0.45,
+            u_innerMult: 0.34,
+            u_inner: true,
+            u_outer: true,
+        },
+    ],
 }
 
 export default shaderInfo
