a0aacf2a6422ed02e3dae3b3b8a45a9c991d89e1
michelle enos <32843306+crankysparrow@users.noreply.github.com>
2025-12-21 10:53:13 -0500

    update build step for workflow

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         }),
         vue(),
     ],
-    base: process.env.NODE_ENV === 'PRODUCTION' ? '/shaders' : '/',
+    // base: process.env.NODE_ENV === 'PRODUCTION' ? '/shaders/' : '/',
     build: {
         rollupOptions: {
             output: {
